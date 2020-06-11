9d8005b63c460523c3d4083fa87c0e0a7b763222
Artem <temaspsxxx@gmail.com>
2020-06-11 16:50:24 +0300

    Add components for app

---
+++ src/components/ArticleList/index.js
@@ -0,0 +1,36 @@
+import React, { PureComponent } from 'react'
+import Article from '../Article'
+import './style.css'
+
+export default class ArticleList extends PureComponent {
+  state = {
+    openArticleId: '56c782f18990ecf954f6e027'
+  }
+
+
+
+  render() {
+    const articleElements = this.props.articles.map((article, index) =>
+      <li key={article.id}
+        className="article-list__li"
+      >
+        <Article
+          article={article}
+          isOpen={this.state.openArticleId === article.id}
+          onButtonClick={this.handleClick.bind(this, article.id)}
+        />
+      </li>
+    )
+
+    return (
+      <ul>
+        {articleElements}
+      </ul >
+    )
+  }
+
+  handleClick = openArticleId => this.setState({
+    openArticleId: this.state.openArticleId === openArticleId ? null : openArticleId
+  })
+
+}
